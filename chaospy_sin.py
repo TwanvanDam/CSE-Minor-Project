2bb9c9bda15dae6be862f96c144b1e2be511114a
Marijn <marijn3@kpnmail.nl>
2023-12-06 13:12:56 +0100

    create evaluations of sin fuction)

--- chaospy_sin.py
+++ chaospy_sin.py
@@ -12,16 +12,16 @@ x = chaospy.Uniform(0, np.pi)
 y = chaospy.Uniform(0, np.pi)
 joint = chaospy.J(x, y)
 
-# Sample joint distribution
-x_samples, y_samples = joint.sample(100, rule="random")
-print(x_samples, y_samples)
+# Sample joint distribution and sample function
+samples = joint.sample(100, rule="random")
+evaluations = sin_data(samples)
 
 # Visualize distribution and sample points
 grid = np.mgrid[0:np.pi:100j, 0:np.pi:100j]
 plt.contourf(grid[0], grid[1], joint.pdf(grid), 30)
-plt.scatter(x_samples, y_samples)
+plt.scatter(*samples)
 plt.show()
 
 # Create polynomial expansion
 expansion = chaospy.generate_expansion(1, joint)
-print(expansion.round(2))
+
