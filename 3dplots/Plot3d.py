9f5c429dbb3120d639338668aaa4de7a97447022
TwanvanDam <118354671+TwanvanDam@users.noreply.github.com>
2023-12-08 16:03:45 +0100

    Updated the proccesing file to output the 0.2% offset yield stress

---
+++ 3dplots/Plot3d.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3.12
+
+import os
+import sys
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.transforms import Affine2D
+from matplotlib.widgets import Slider
+import numpy as np 
+import plotly.graph_objects as go
+
+
+def load_data(file_prefix,num_layers):
+    #initialize array 
+    bool_array = np.zeros((100,100,num_layers),dtype=bool)
+    for i in range(0,num_layers):
+        img = imageio.imread(f"{file_prefix}_{i:04d}.tif")
+        bool_array[:,:,i] = img!=0 
+    array_sum = np.sum(bool_array)
+    return bool_array, array_sum
+
+def clean_data(bool_array,downsample_size=1):
+    bool_array = bool_array[::downsample_size,::downsample_size,::downsample_size]
+    small_array = np.copy(bool_array)
+    for z in range(1,bool_array.shape[2]-1): #z dimension
+        for y in range(1,bool_array.shape[1]-1): #y dimension
+            for x in range(1,bool_array.shape[0]-1): #x dimension
+                if (bool_array[x,y,z])  and (bool_array[x-1,y,z]) and (bool_array[x+1,y,z]) and (bool_array[x,y+1,z]) and (bool_array[x,y-1,z]) and (bool_array[x,y,z-1]) and (bool_array[x,y,z+1]):
+                    small_array[x,y,z] = False
+    small_array_sum = np.sum(small_array)
+    return small_array, small_array_sum
+
+def plot_3d_volume(bool_array):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.axis('off')
+    ax.voxels(filled=bool_array)
+    plt.show()
+    return
+    
+def plot_like_mri(vol):
+    volume = vol.T
+    print(volume.shape)
+    r, c = volume[0].shape
+
+    # Define frames
+    import plotly.graph_objects as go
+    nb_frames = 100
+
+    fig = go.Figure(frames=[go.Frame(data=go.Surface(
+        z=(10 - k * 0.1) * np.ones((r, c)),
+        surfacecolor=np.flipud(volume[99 - k]),
+        cmin=0, cmax=1
+        ),
+        name=str(k) # you need to name the frame for the animation to behave properly
+        )
+        for k in range(nb_frames)])
+
+    # Add data to be displayed before animation starts
+    fig.add_trace(go.Surface(
+        z=6.7 * np.ones((r, c)),
+        surfacecolor=np.flipud(volume[67]),
+        colorscale='Gray',
+        cmin=0, cmax=200,
+        colorbar=dict(thickness=20, ticklen=4)
+        ))
+
+
+    def frame_args(duration):
+        return {
+                "frame": {"duration": duration},
+                "mode": "immediate",
+                "fromcurrent": True,
+                "transition": {"duration": duration, "easing": "linear"},
+            }
+
+    sliders = [
+                {
+                    "pad": {"b": 10, "t": 60},
+                    "len": 0.9,
+                    "x": 0.1,
+                    "y": 0,
+                    "steps": [
+                        {
+                            "args": [[f.name], frame_args(0)],
+                            "label": str(k),
+                            "method": "animate",
+                        }
+                        for k, f in enumerate(fig.frames)
+                    ],
+                }
+            ]
+
+    # Layout
+    fig.update_layout(
+             title='Slices in volumetric data',
+             width=600,
+             height=600,
+             scene=dict(
+                        zaxis=dict(range=[-0.1, 10], autorange=False),
+                        aspectratio=dict(x=1, y=1, z=1),
+                        ),
+             updatemenus = [
+                {
+                    "buttons": [
+                        {
+                            "args": [None, frame_args(50)],
+                            "label": "&#9654;", # play symbol
+                            "method": "animate",
+                        },
+                        {
+                            "args": [[None], frame_args(0)],
+                            "label": "&#9724;", # pause symbol
+                            "method": "animate",
+                        },
+                    ],
+                    "direction": "left",
+                    "pad": {"r": 10, "t": 70},
+                    "type": "buttons",
+                    "x": 0.1,
+                    "y": 0,
+                }
+             ],
+             sliders=sliders
+    )
+
+    fig.show()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 1:
+        file_prefix =  f"~/Downloads/20230206_Scan001_100px/Image Stacks/Scan{sys.argv[1]}/Scan{sys.argv[1]}"
+    else:
+        file_prefix = "~/Downloads/20230206_Scan001_100px/Image Stacks/Scan001_006/Scan001_006"
+
+    if len(sys.argv) == 3:
+        downsample_size = int(sys.argv[2])
+    else:
+        downsample_size = 5
+    
+    num_layers = 100
+    data, array_sum = load_data(file_prefix,100)
+    data_small, small_array_sum = clean_data(data,downsample_size)
+    print(small_array_sum,array_sum)
+    print(f"Decreased the datasize by {(1-small_array_sum/ array_sum)*100}%")
+    #plot_like_mri(data)
+    plot_3d_volume(data_small)
+    
+    
--- max_derivative_ML.py
+++ max_derivative_ML.py
@@ -5,7 +5,6 @@ import numpy as np
 import torch
 import gpytorch
 import matplotlib.pyplot as plt
-import matplotlib
 import time
 from alive_progress import alive_bar
 
@@ -243,6 +242,177 @@ def computeMaxEnergyDerivative(fig_filename, csv_filename, degree=10, do_show=Tr
         return None
     else:
         return stress_array_true[i_max]
+    
+def computeMaxEnergyDerivative2(fig_filename, csv_filename, degree=10, do_show=True,confidence=False,training_size=100,verbose=True,iterations=500):
+    ''' Return location (in strain) of maximum of energy derivative
+        by approximating the Energy with Gaussian Process (which is now derivable)
+        in order to find a precise location (on the approximation)
+    '''
+    data = parseCsvFile(csv_filename,verbose=verbose)
+    loading_vel = 1 #mm/min
+    sample_height = 1 #mm
+    sample_diameter = 1 #mm
+    # recording_interval = 2 #s
+    strain_adjust = 0 #0.01647
+    area = math.pi*pow(sample_diameter/2,2)
+
+    #Iterations of bayesian regression (loss should be minimal)
+    training_iter = iterations
+
+    strain_array = np.array(data['time'])/max(np.array(data['time']))
+    stress_array = -np.array(data['stress_11_top'])/max(-np.array(data['stress_11_top']))
+    energy_array = strain_array * stress_array
+
+    der_energy_array = []
+    ##discrete derivative
+    for i in range(0,len(energy_array)-1):
+      der_energy_array.append((energy_array[i+1]-energy_array[i])/(strain_array[i+1]-strain_array[i]))
+
+    train_x = torch.from_numpy(strain_array[:training_size]).float()
+    train_y = torch.from_numpy(energy_array[:training_size]).float()
+
+    # We will use the simplest form of GP model, exact inference
+    class ExactGPModel(gpytorch.models.ExactGP):
+        def __init__(self, train_x, train_y, likelihood):
+            super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+            self.mean_module = gpytorch.means.ConstantMean()
+            self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+        def forward(self, x):
+            mean_x = self.mean_module(x)
+            covar_x = self.covar_module(x)
+            return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+    # initialize likelihood and model
+    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-7,1e3))
+    model = ExactGPModel(train_x, train_y, likelihood)
+
+    # Find optimal model hyperparameters
+    model.train()
+    likelihood.train()
+
+    # Use the adam optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
+
+    # "Loss" for GPs - the marginal log likelihood
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+    # with gpytorch.settings.cholesky_jitter(1e-3):
+    for i in range(training_iter):
+        # Zero gradients from previous iteration
+        optimizer.zero_grad()
+        # Output from model
+        output = model(train_x)
+        # Calc loss and backprop gradients
+        loss = -mll(output, train_y)
+        loss.backward()
+        if verbose:
+            print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.8f' % (
+                i + 1, training_iter, loss.item(),
+                model.covar_module.base_kernel.lengthscale.item(),
+                model.likelihood.noise.item()
+        ))
+        optimizer.step()
+
+    # Get into evaluation (predictive posterior) mode
+    model.eval()
+    likelihood.eval()
+
+    test_x = torch.linspace(0, 1, 250)
+
+    # Test points are regularly spaced along [0,1]
+    # Make predictions by feeding model through likelihood
+    with torch.no_grad(), gpytorch.settings.fast_pred_var():
+        observed_pred = likelihood(model(test_x))
+    """
+    if confidence:
+        with torch.no_grad():
+            # Initialize plot
+            f, ax = plt.subplots(1, 1, figsize=(4, 3))
+    
+            # Get upper and lower confidence bounds
+            lower, upper = observed_pred.confidence_region()
+            # Plot training data as black stars
+            ax.plot(train_x.numpy(), train_y.numpy(), 'k*')
+            # Plot predictive means as blue line
+            ax.plot(test_x.numpy(), observed_pred.mean.numpy(), 'b')
+            # Shade between the lower and upper confidence bounds
+            ax.fill_between(test_x.numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
+            # ax.set_ylim([-3, 3])
+            ax.legend(['Observed Data', 'Mean', 'Confidence'])
+    
+            plt.show()"""
+
+    # test_x = torch.linspace(0.02, 0.04, 1000)
+    X = torch.autograd.Variable(torch.Tensor(test_x), requires_grad=True)
+    observed_pred = likelihood(model(X))
+
+    y = observed_pred.mean.sum()
+    y.backward()    
+    dydtest_x = X.grad
+
+    ##Outputs
+    test_x_np = test_x.numpy()
+    derivative = dydtest_x.numpy()
+    
+    i_max = np.argmax(derivative[:100])
+
+    strain_array_true = strain_array * max(np.array(data['time']))/sample_height #- 0.006384
+    test_x_np_true = test_x_np * max(np.array(data['time']))/sample_height #- 0.006384
+    stress_array_true = stress_array * max(-np.array(data['stress_11_top'])) * 1000 / area
+    
+    gp_stress = (observed_pred.mean.detach().numpy()/ test_x) * (max(-np.array(data['stress_11_top']))) * 1000 / area
+    
+    yield_strain1, offset_stress1 = get_yield(stress_array_true, strain_array_true,offset=0.001)
+    yield_strain2, offset_stress2 = get_yield(stress_array_true, strain_array_true,offset=0.002)
+    yield_strain2gp, offset_stress2gp = get_yield(gp_stress,test_x_np_true,offset=0.002)
+    
+    """
+    fig = P.figure(figsize=[6.4, 8])
+    # first subplot: strain-stress
+    ax = fig.add_subplot(211)
+    plt.subplots_adjust(bottom=0.08, top=0.95, hspace=0.1)
+    plt.plot(strain_array_true, stress_array_true, '.',label="FEM datapoints",color=(0.5,0.5,0.5))
+    plt.plot(test_x_np_true,gp_stress,'k-',label="GP approximation")
+    plt.plot(strain_array_true[:int(1500*yield_strain2)],offset_stress2[:int(1500*yield_strain2)],label=f"offset stress",color=(0.5,0.5,0.5))
+    plt.axvline(x=test_x_np_true[i_max],label="GP yield point",color='r')
+    plt.axvline(yield_strain1,label="Classic 0.1% yield point",color='g')
+    plt.axvline(yield_strain2,label="Classic 0.2% yield point",color='b')
+    # plt.xlim(0,0.040)
+    # plt.ylim(0,40)
+    plt.ylabel('Top stress (MPa)', fontsize=15)
+    plt.legend()
+    # Second subplot: derivative energy (approximation)
+    ax = fig.add_subplot(212)
+    plt.plot(strain_array_true[:-1], der_energy_array, color=(0.5, 0.5, 0.5), linewidth=0.8, linestyle='solid', label='Raw discrete derivative')
+    plt.plot(test_x_np_true[:100], derivative[:100], color=(0, 0, 0), linewidth=1.2, linestyle='solid', label='GP approximation')
+    plt.axvline(x=test_x_np_true[i_max],label="GP yield point",color='r')
+    plt.axvline(yield_strain1,label="Classic 0.1% yield point",color='g')
+    plt.axvline(yield_strain2,label="Classic 0.2% yield point",color='b')
+    # plt.xlim(0,0.040)
+    # plt.ylim(-20,140)
+    plt.yticks([])
+    plt.xlabel('Vertical Strain', fontsize=15)
+    plt.ylabel('Mechanical work derivative', fontsize=15)
+    plt.legend()
+
+    if not do_show:
+        plt.savefig("Results/Plots/Max/"+fig_filename, format='png', dpi=300)
+        print(f'Figure saved as {fig_filename}')
+        plt.close()
+    else:
+        plt.show()"""
+    return stress_array_true[i_max], stress_array_true[strain_array_true==yield_strain1][0], stress_array_true[strain_array_true==yield_strain2][0]
+
+def get_yield(stress,strain,offset=0.002):
+    slope = (stress[20]-stress[0])/(strain[20]-strain[0])
+
+    offset_stress = slope*(strain-offset)
+
+    yield_index = np.argmin(np.abs(stress-offset_stress))
+    yield_strain = strain[yield_index]
+
+    return yield_strain, offset_stress
 
 def output_yield_points(output_title,start,end):
     """Function outputs the yield points of all the FEM Simulations"""
@@ -260,16 +430,13 @@ def output_yield_points(output_title,start,end):
                             output.writelines(f"{csv_filename}, yield = None, {time.time()-start:.1f}\n") 
                             os.rename(data_dir+csv_filename,data_dir+"Weird_Data/"+csv_filename)
                     bar()
-def make_plots(filename,show=True,training_size=100):
-    computeMaxEnergyDerivative(filename
-                               .replace(".csv",".png"),filename
-                               ,do_show=show, verbose=True,save=True,training_size=training_size)
-    return
+def make_plots(filename,show=True,training_size=100,verbose=True,confidence=False,iterations=500):
+    gpyield, yield_stress1, yield_stress2 = computeMaxEnergyDerivative2(filename.replace(".csv",".png"),filename,training_size=training_size,verbose=verbose,do_show=show,confidence=confidence,iterations=iterations)
+    return gpyield, yield_stress1, yield_stress2
 
 
 if __name__ == '__main__':
-    cwd = os.getcwd() #Get the current directory
-    data_dir = cwd + "/data/FEMResults/" #Put the data in this directory
+    data_dir = "./data/FEMResults/" #Put the data in this directory
     verbose = False
     num_files = len(os.listdir(data_dir))
     output_title = "Results/output.txt"
@@ -281,12 +448,25 @@ if __name__ == '__main__':
         output_title = sys.argv[3]
     #print(f"Calculating yield points for files {start_file} - {end_file} in the data directory and saving in "+output_title)
     #output_yield_points(output_title,start_file,end_file)
-    """with alive_bar(end_file) as bar:
-        for i in range(end_file):
-            if data_dir[i] != "Weird_Data":
-                make_plots(os.listdir(data_dir)[i],show=False,training_size=150)
-            bar()"""
-    make_plots("Scan001_005.csv",training_size=100)
+    
+    sum1 = 0
+    sum2 = 0
+    all = True
+    
+    if all:
+        with open("output.txt",'w') as output:
+            with alive_bar(end_file) as bar:
+                print(len(os.listdir(data_dir)))
+                for file in os.listdir(data_dir)[start_file:end_file]:
+                    if file != "Weird_Data":
+                        gpyield, yield1, yield2 = make_plots(file,show=False,training_size=100,verbose=verbose)
+                        print(f"gp predicts:{gpyield}, 0.1% ofsset:{yield1}, 0.2 %offset:{yield2}")
+                        output.writelines(f"{file}, {yield2}\n")
+                        sum2 += abs(gpyield - yield2)
+                        sum1 += abs(gpyield - yield1)
+                    bar()
+    else:
+        make_plots("Scan001_005.csv",training_size=200,confidence=True,iterations=500)
 
 
 #TODO
