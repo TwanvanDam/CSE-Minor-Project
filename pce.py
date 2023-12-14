891f06416bd5e8b5c8b049ebeca9ef15f9110060
Marijn <marijn3@kpnmail.nl>
2023-12-14 01:50:06 +0100

    add function to read data file

---
+++ pce.py
@@ -0,0 +1,19 @@
+import numpy as np
+import chaospy
+import csv
+
+def read_data(sample_fieldnames, evaluation_fieldname, path):
+    samples = [[] for i in range(len(sample_fieldnames))]
+    evaluations = []
+    with open(path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            evaluations.append(row[evaluation_fieldname])
+            for element, fieldname in zip(samples, sample_fieldnames):
+                element.append(row[fieldname])
+
+    samples = np.array(samples, dtype=np.float64)
+    evaluations = np.array(evaluations, dtype=np.float64)
+    return samples, evaluations
+
+samples, evaluations  = read_data(["x", "y"], "sin(x)", "data/sin_data.csv")
